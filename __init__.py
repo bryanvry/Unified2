eca54c644b2ac0effe998b302ee475c4a65c8644
bryanvry <bryanvrya@gmail.com>
2025-11-12 04:19:38 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+
+from .unified_parser import UnifiedParser
+from .southern_glazers import SouthernGlazersParser
+from .nevada_beverage import NevadaBeverageParser
+
+ALL_PARSERS = [UnifiedParser(), SouthernGlazersParser(), NevadaBeverageParser()]
